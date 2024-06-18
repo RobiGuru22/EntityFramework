2717024223f0c6e768633c44a24e49aae117a5ac
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-18 09:33:36 +0200

    Initialization:DatabaseConenction
    ConfiguratioNManager, és CnnHelper.cs hozzáadva

---
+++ EntityFrameworkPractice/EntityFrameworkPractice/DatabaseAccess/CnnHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.IdentityModel.Protocols;
+
+namespace EntityFrameworkPractice.DatabaseAccess
+{
+    public class CnnHelper
+    {
+        public static string GetCnnString(string dbName)
+        {
+            return ConfigurationManager.ConnectionStrings[dbName].ConnectionString;   
+        }
+    }
+}
