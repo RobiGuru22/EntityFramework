6f8cd3912c118ceb3350f5e069c008b494ed6d0c
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-18 09:37:47 +0200

    Update:TrackerContext.cs
    CnnHelper.cs használata connection string eléréshez

--- EntityFrameworkPractice/EntityFrameworkPractice/Data/TrackerContext.cs
+++ EntityFrameworkPractice/EntityFrameworkPractice/Data/TrackerContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using EntityFrameworkPractice.DatabaseAccess;
 using EntityFrameworkPractice.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -19,8 +20,9 @@ public partial class TrackerContext : DbContext
     public virtual DbSet<Invoice> Invoices { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=RET001377\\MYSERVER;Database=Tracker;User Id=sa;Password=InkiPinkiPonky.;TrustServerCertificate=true");
+    {
+        optionsBuilder.UseSqlServer(CnnHelper.GetCnnString("MyServer"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
