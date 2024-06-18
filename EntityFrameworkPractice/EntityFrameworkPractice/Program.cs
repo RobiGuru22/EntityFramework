e1db5b81d60c1fff5194eb64de72e79617192ec5
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-18 18:00:07 +0200

    QueryState:Insertion_Removal added
    Supplier insert és remove hozzáadva, fő programban kipróbálva

--- EntityFrameworkPractice/EntityFrameworkPractice/Program.cs
+++ EntityFrameworkPractice/EntityFrameworkPractice/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using EntityFrameworkPractice.Models;
+using EntityFrameworkPractice;
+using EntityFrameworkPractice.Data;
+
+/*Supplier supplier = new Supplier
+{
+    Name = "Teszt Szup 1",
+    GLN = "1111111111222",
+    TaxID = "1111-22-33",
+    AccountNumber = "11111111-22222222-33333333",
+    CodeByBuyer = "11112222",
+    PublicAreaName = "Teszt utca 1.",
+    CityName = "Teszt város",
+    PostalCode = "1234",
+    Country = "HU"
+};*/
+
+using TrackerContext context = new TrackerContext();
+
+var deletable = context.Suppliers.Where(x => x.Name == "Teszt Szup 1").FirstOrDefault();
+
+if(deletable is Supplier)
+{
+    Querys.RemoveSupplier(deletable);
+}
+
+
+Console.WriteLine("Removal done!");
+
---
+++ EntityFrameworkPractice/EntityFrameworkPractice/Querys.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkPractice.Data;
+using EntityFrameworkPractice.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPractice
+{
+    public class Querys
+    {
+        public static void InsertSupplier(Supplier supplier)
+        {
+            using TrackerContext context = new TrackerContext();
+            context.Suppliers.Add(supplier);
+            context.SaveChanges();
+        }
+        public static void RemoveSupplier(Supplier supplier)
+        {
+            using TrackerContext context = new TrackerContext();
+            context.Suppliers.Remove(supplier);
+            context.SaveChanges();
+        }
+    }
+}
