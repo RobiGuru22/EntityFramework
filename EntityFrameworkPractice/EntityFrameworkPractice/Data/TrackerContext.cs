36faee811aa2888ed621b6f439e3400b52ba3294
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-18 08:22:05 +0200

    Initialization:GettingBaseDatabase
    PMC-ben Scaffold-al alap adatbázis beemelése, ezzel Context.cs, és egy model .cs létrejött

---
+++ EntityFrameworkPractice/EntityFrameworkPractice/Data/TrackerContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using EntityFrameworkPractice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkPractice.Data;
+
+public partial class TrackerContext : DbContext
+{
+    public TrackerContext()
+    {
+    }
+
+    public TrackerContext(DbContextOptions<TrackerContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Invoice> Invoices { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=RET001377\\MYSERVER;Database=Tracker;User Id=sa;Password=InkiPinkiPonky.;TrustServerCertificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Invoice>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ EntityFrameworkPractice/EntityFrameworkPractice/Models/Invoice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameworkPractice.Models;
+
+public partial class Invoice
+{
+    public int? Id { get; set; }
+
+    public int? InvoiceNumber { get; set; }
+
+    public string? TestSample { get; set; }
+}
