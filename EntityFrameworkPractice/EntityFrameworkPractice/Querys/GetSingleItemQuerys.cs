9ef32ec868878446da1ad1806bcd669823ae72cb
RobiGuru22 <hoorvath.robert@gmail.com>
2024-06-18 18:25:19 +0200

    QueryState:GetSingleItemQuerys added
    Retailer és Suppler GLN alapján való lehívása megvalósítva null reference lehetőséggel

---
+++ EntityFrameworkPractice/EntityFrameworkPractice/Querys/GetSingleItemQuerys.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityFrameworkPractice.Data;
+using EntityFrameworkPractice.Models;
+
+namespace EntityFrameworkPractice
+{
+    public class GetSingleItemQuerys
+    {
+        public static Retailer? GetRetailerByGLN(string gln)
+        {
+            using TrackerContext context = new TrackerContext();
+            return context.Retailers.Where(x => x.GLN == gln).FirstOrDefault();
+        }
+        public static Supplier? GetSupplierByGLN(string gln)
+        {
+            using TrackerContext context = new TrackerContext();
+            return context.Suppliers.Where(x => x.GLN == gln).FirstOrDefault();
+        }
+    }
+}
